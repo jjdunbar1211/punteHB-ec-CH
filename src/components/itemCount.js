22a81ace2c3cc028e04ede7e34bb67bdde0eb9ca
jjdunbar1211 <csamignone@gmail.com>
2021-01-18 19:34:50 -0300

    Desafío nro.9 - Deshabilito +/- del ItemCount cuando ya agregué los productos al cart

--- src/components/itemCount.js
+++ src/components/itemCount.js
@@ -5,11 +5,11 @@ const ItemCount = ({ add, substract, agregarAlCarrito, item, counter, isCartRead
     return (
         <div className="flex flex-col ">  
             <div className="flex justify-around">
-                <button onClick={ substract }  >
+                <button onClick={ substract }  disabled={isCartReady}>
                     <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
                 </button>
                 <p >{ counter }</p>
-                <button onClick={ add }  >
+                <button onClick={ add }  disabled={isCartReady}>
                     <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
                 </button>
             </div>
