c2cbcd03e85f8c25e82a6c0bc9caf35d66ea528a
jjdunbar1211 <csamignone@gmail.com>
2021-02-01 15:26:59 -0300

    Desafío nro. 13

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,41 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {CartContext} from './CartContext';
 import { Link } from 'react-router-dom';
+import {firestore} from '../firebase';
+import firebase from 'firebase/app';
 
 const Cart = () => {
 
     const {cart, clearCart,eliminateFromCart, total} = useContext(CartContext);
+
+    // 
+    const [ openPay, setOpenPay ] = useState(false)
+    // campos que me piden para la orden de compra
+    const [ name, setName ] = useState('')
+    const [ phone, setPhone ] = useState()
+    const [ email, setEmail ] = useState()
+
+    const submitOrder = () => {
+
+        const db = firestore
+        const orders = db.collection('orders')
+
+        const order = {
+            buyer: { name: name, phone: phone, email: email},
+            items : cart.map(function(obj){
+                return {id: obj.id, name: obj.name, price: obj.price, quantity: obj.amount} ;
+             }),
+            date: firebase.firestore.Timestamp.fromDate(new Date()),
+            total: total,
+        }
+
+        orders.add(order)
+            .then(({ id }) => alert("Id de tu compra: " + id))
+            .catch((error) => console.log(error))
+
+        setOpenPay(false)
+        clearCart()
+    }
     
     return (
     <div >
@@ -41,7 +72,8 @@ const Cart = () => {
             </div>  
             <div className="mt-4 flex justify-center">
                 {/* falta implementar la simulación de la compra */}
-                <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+                <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+                        onClick={() => {setOpenPay(true)}} >
                     COMPRARRR
                 </button>
             </div>    
@@ -56,6 +88,43 @@ const Cart = () => {
             </Link>
         </div>)
         }
+        { openPay && 
+            <form onSubmit={submitOrder}>
+                <div className="flex justify-center mt-4 ">
+                    <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+                        <div className="mb-4">
+                          <label className="block text-grey-darker text-sm font-bold mb-2" for="username">
+                            Nombre
+                          </label>
+                          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="username" type="text" placeholder="Nombre"
+                                 onChange={(e) => setName(e.target.value)} />
+                        </div>
+                        <div className="mb-4">
+                          <label className="block text-grey-darker text-sm font-bold mb-2" for="phone">
+                            Teléfono
+                          </label>
+                          <input className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3" id="phone" type="number" placeholder="11 11111" 
+                                 onChange={(e) => setPhone(e.target.value)} />
+                        </div>
+                        <div className="mb-4">
+                          <label className="block text-grey-darker text-sm font-bold mb-2" for="email">
+                            Email
+                          </label>
+                          <input className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3" id="email" type="text" placeholder="micompra@gmail.com" 
+                                 onChange={(e) => setEmail(e.target.value)} />
+                        </div> 
+                        <div className="flex  ">
+                            <Link to="/">       
+                                <button className="hover:bg-gray-100 text-black font-bold py-2 px-4 rounded border border-gray-400 shadow" type="button" 
+                                        onClick={submitOrder} >
+                                  Click aquí para terminar!
+                                </button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+          </form>}
+
     </div>
     )
 }
