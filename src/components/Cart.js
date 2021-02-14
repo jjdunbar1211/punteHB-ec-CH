73a99e7823a3021fae0d8f2b39cc17f02c796bbe
jjdunbar <csamignone@gmail.com>
2021-02-14 05:32:48 -0300

    detalles tabla compra

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -67,8 +67,8 @@ const Cart = () => {
         { cart.length > 0 ? 
          <div>    
             <div className="mt-4 flex justify-center ">
-                <table className="table-auto shadow bg-gray-100">
-                    <thead>
+                <table className="table-auto shadow bg-gray-100 border-2 border-gray-800">
+                    <thead className="border-b-2 border-gray-800">
                         <tr>
                             <th className="w-1/5">Artículo</th>
                             <th className="w-1/5">Cantidad</th>
@@ -84,16 +84,18 @@ const Cart = () => {
                                 <td className="text-center">{p.amount}</td>
                                 <td className="text-center">${p.price}</td>
                                 <td className="text-center">${p.price * p.amount}</td>
-                                <td className="flex justify-center ">
-                                   <button onClick={() => {eliminateFromCart(p.id)}}>
-                                      <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"></path></svg>
-                                   </button>
+                                <td className="text-center">
+                                    <div className="flex-col justify-center">
+                                        <button className=" flex-col justify-center transform hover:scale-125 transition ease-out duration-200" onClick={() => {eliminateFromCart(p.id)}}>
+                                           <svg className="w-6 h-6 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"></path></svg>
+                                        </button>
+                                    </div>
                                 </td>
                         </tr>))
                     }                                 
                     </tbody>
                 </table> 
-                <p className="bg-gray-100 text-lg font-bold shadow self-end">{`Total: $${total}`}</p>
+                <p className="bg-gray-100 border-b-2 border-t-2 border-r-2 border-gray-800 text-lg font-bold self-end">{`Total: $${total}`}</p>
             </div>  
             <div className="mt-4 flex justify-center">
                 <div hidden={openPay}>
