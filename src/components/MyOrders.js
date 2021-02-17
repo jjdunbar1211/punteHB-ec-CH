005d39e9bafccadba18d9250be743f4f298191f8
jjdunbar <csamignone@gmail.com>
2021-02-16 23:25:33 -0300

    Proyecto eCommerce para PunteHB - Entrega final

---
+++ src/components/MyOrders.js
@@ -0,0 +1,30 @@
+/* import { useState } from "react"
+
+const MyOrders = () => {
+
+    const [idOrder, setIdOrder] = useState()
+
+    const searchOrder = () => {
+        
+    }
+
+    return ( 
+        <div className="mt-4">
+            <h1 className="text-center font-semibold"> - Mis compras - </h1>
+                <div className="flex justify-center items-center">    
+                <div className="mb-4">
+                        <label htmlFor="myordees" className="block text-grey-darker text-sm font-bold mb-2">Ingresá el código de tu orden</label>
+                        <input type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="myorders" value={idOrder}
+                               onChange={ evt =>  setIdOrder(evt.target.value)} />
+                        <button type="submit" onClick={searchOrder}>
+                                Buscar
+                        </button>
+                </div>
+                        
+                </div>
+          </div>
+     )
+
+}
+ 
+export default MyOrders */
